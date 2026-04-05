d45f9910057077c14913594f00fd5af81fd5c0c0
Yudai Morimoto <legolesia@gmail.com>
2026-04-05 23:28:48 +0900

    feat:5-9

--- os/run.sh
+++ os/run.sh
@@ -11,7 +11,7 @@ CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld
 
 #　カーネルをビルド
 $CC $CFLAGS -Wl,-Tos/kernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    os/kernel.c
+    os/kernel.c os/common.c
 
 
 # QEMUを起動
